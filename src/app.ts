11ff8d3d021177bda4476d5f48d8dc0b3406157f
Yeamin Foysal <yeaminfoysal14@gmail.com>
2025-07-09 01:10:47 +0600

    Enable CORS for all origins

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from 'express'
 import { bookRoutes } from './app/controllers/books.controller';
-import { borrowRoutes } from './app/controllers/borrow.controller';
+import { borrowRoutes } from './app/controllers/borrow.controller'
+const cors = require('cors');
 
 const app: Application = express();
 
 app.use(express.json());
+// Enable CORS for all origins
+app.use(cors());
 
 app.use('/api/books', bookRoutes);
 app.use('/api/borrow', borrowRoutes);
