766adc0ef2efc78aeb77a11d48fe7c7f16f661a9
Yeamin Foysal <yeaminfoysal14@gmail.com>
2025-07-09 01:16:27 +0600

    updated tsc

--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const books_controller_1 = require("./app/controllers/books.controller");
 const borrow_controller_1 = require("./app/controllers/borrow.controller");
+const cors = require('cors');
 const app = (0, express_1.default)();
+// Enable CORS for all origins
+app.use(cors());
 app.use(express_1.default.json());
 app.use('/api/books', books_controller_1.bookRoutes);
 app.use('/api/borrow', borrow_controller_1.borrowRoutes);
