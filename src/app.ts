171202e15bb960da72d6de01874e777040915467
Yeamin Foysal <yeaminfoysal14@gmail.com>
2025-07-09 01:13:57 +0600

    Enable CORS for all origins

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,11 @@ const cors = require('cors');
 
 const app: Application = express();
 
-app.use(express.json());
 // Enable CORS for all origins
 app.use(cors());
+
+app.use(express.json());
+
 
 app.use('/api/books', bookRoutes);
 app.use('/api/borrow', borrowRoutes);
